6d484426ce3d29ca472cf8272dbf72ff66eeff52
Tony Cleem <tonycleem@gmail.com>
2025-03-26 23:37:19 +0700

    move queries into QuerySet methods

--- blog/views.py
+++ blog/views.py
@@ -31,18 +31,9 @@ def index(request):
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
 
-
-    most_popular_posts = Post.objects.prefetch_related(
-        'author',
-    'tags').annotate(likes_count=Count('likes')).order_by('-likes_count')[:5]
-    most_popular_posts_ids = [post.id for post in most_popular_posts]
-
-    posts_with_comments = Post.objects.filter(id__in=most_popular_posts_ids).annotate(comments_count=Count('comments'))
-    ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
-    count_for_id = dict(ids_and_comments)
-
-    for post in most_popular_posts:
-        post.comments_count = count_for_id[post.id]
+    most_popular_posts = Post.objects.popular() \
+            .prefetch_related('author', 'tags')[:5] \
+            .fetch_with_comments_count()
 
     most_popular_tags = Tag.objects.popular()[:5]
 
